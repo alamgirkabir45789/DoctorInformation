e015f15b071b819041246571c99c34909a1daa2b
Alamgir Kabir <64708275+alamgirkabir45789@users.noreply.github.com>
2021-02-01 19:03:22 +0600

    Add project files.

---
+++ DoctorInformation/Doctor.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace DoctorInformation
+{
+    public class Doctor : INotifyPropertyChanged
+    {
+        public int _doctorID;
+        public int DoctorID
+        {
+            get => this._doctorID;
+            set
+            {
+                this._doctorID = value;
+                this.OnPropertyChanged(nameof(DoctorID));
+            }
+        }
+
+        public string _name;
+        public string Name
+        {
+            get => this._name;
+            set
+            {
+                this._name = value;
+                this.OnPropertyChanged(nameof(Name));
+
+            }
+        }
+
+        public string _address;
+        public string Address
+        {
+            get => this._address;
+            set
+            {
+                this._address = value;
+                this.OnPropertyChanged(nameof(Address));
+
+            }
+        }
+
+
+        public string _phone;
+        public string Phone
+        {
+            get => this._phone;
+            set
+            {
+                this._phone = value;
+                this.OnPropertyChanged(nameof(Phone));
+
+            }
+        }
+
+        public string _email;
+        public string Email
+        {
+            get => this._email;
+            set
+            {
+                this._email = value;
+                this.OnPropertyChanged(nameof(Email));
+
+            }
+        }
+
+        public string _specialization;
+
+
+        public string Specialization
+        {
+            get => this._specialization;
+            set
+            {
+                this._specialization = value;
+                this.OnPropertyChanged(nameof(Specialization));
+
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+    }
+}
---
+++ DoctorInformation/ViewModel.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace DoctorInformation
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        private List<Doctor> doctors;
+
+        public Command NextDoctor { get; private set; }
+        public Command PreviousDoctor { get; private set; }
+
+
+        private int currentDoctor;
+        public ViewModel()
+        {
+
+            this.currentDoctor = 0;
+            this.IsAtStart = true;
+            this.IsAtEnd = false;
+
+            this.NextDoctor = new Command(this.Next, () => this.doctors.Count > 1 && !this.IsAtEnd);
+
+            this.PreviousDoctor = new Command(this.Previous, () => this.doctors.Count > 0 && !this.IsAtStart);
+
+
+
+            this.doctors = new List<Doctor>
+            {
+                new Doctor
+                {
+                    DoctorID=1,
+                    Name="Alamgir",
+                    Address="Dinajpur",
+                    Phone="0121212111",
+                    Email="alam@gmail.com",
+                    Specialization="Cardiac"
+            
+                },
+
+                new Doctor
+                {
+                    DoctorID=2,
+                    Name="Shabana",
+                    Address="Chittagong",
+                    Phone="0158555",
+                    Email="shabana@gmail.com",
+                    Specialization="Surgery"
+
+                },
+
+                new Doctor
+                {
+                    DoctorID=3,
+                    Name="Mizan",
+                    Address="Pabna",
+                    Phone="01255111",
+                    Email="mizan@gmail.com",
+                    Specialization="Medicine"
+
+                }
+
+
+
+            };
+        }
+
+
+        private bool _isAtStart;
+        public bool IsAtStart
+        {
+            get => this._isAtStart;
+            set
+            {
+                this._isAtStart = value;
+                this.OnPropertyChanged(nameof(IsAtStart));
+            }
+        }
+
+        private bool _isAtEnd;
+        public bool IsAtEnd
+        {
+            get => this._isAtEnd;
+            set
+            {
+                this._isAtEnd = value;
+                this.OnPropertyChanged(nameof(IsAtEnd));
+            }
+        }
+
+
+        public Doctor Current
+        {
+            get => this.doctors.Count > 0 ? this.doctors[currentDoctor] : null;
+        }
+
+        private void Next()
+        {
+            if (this.doctors.Count - 1 > this.currentDoctor)
+            {
+                this.currentDoctor++;
+                this.OnPropertyChanged(nameof(Current));
+                this.IsAtStart = false;
+                this.IsAtEnd = (this.doctors.Count - 1 == this.currentDoctor);
+            }
+        }
+
+        private void Previous()
+        {
+            if (this.doctors.Count > 0)
+            {
+                this.currentDoctor--;
+                this.OnPropertyChanged(nameof(Current));
+                this.IsAtEnd = false;
+                this.IsAtStart = (this.currentDoctor == 0);
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
